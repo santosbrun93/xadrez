3e2ed8b9785ca3c7e1f1ef70e6d2dd249593b8fa
Bruno Santos <santos.brun93@gmail.com>
2023-07-01 18:50:30 -0300

    Criação das classes base para o tabuleiro

--- xadrez_console_c/Program.cs
+++ xadrez_console_c/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tabuleiro;
+using System;
+
+namespace xadrez_console_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            Peca peca = new Peca(p, Cor.Preta, tab);
+
+            Console.WriteLine("Posição: " + p.ToString());
+            Console.WriteLine("Tabuleiro: " + tab.ToString());
+            Console.WriteLine("Peca: " + peca.ToString());
+        }
+    }
+
+}
---
+++ xadrez_console_c/Tabuleiro/Peca.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+
+        public Cor cor { get; protected set; }
+
+        public int qtdMovimentos { get; protected set ; }
+
+        public Tabuleiro tab { get; protected set; }
+
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            this.cor = cor;
+            this.tab = tab;
+            this.qtdMovimentos = 0;
+        }
+
+        public override string ToString()
+        {
+            return Posicao + ", " + cor + ", "+ tab;
+        }
+    }
+}
